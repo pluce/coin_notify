ea6b383f58e1c0929fb9e1b3cd98daf00f817d92
Raphael TIN <ratin@e2r3p1.42.fr>
2019-12-02 06:41:54 +0100

    [SEND_MAIL] added real mail message

--- coin_notify/scripts/send_mail.py
+++ coin_notify/scripts/send_mail.py
@@ -1,5 +1,6 @@
 from alert import models
 import requests
+import inspect
 from django.core.mail import send_mail
 
 def get_rate_BTC():
@@ -9,26 +10,33 @@ def get_rate_BTC():
     url = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD'
     headers = {'X-CoinAPI-Key' : 'BF00CAD7-3171-4359-9FF3-AE20F922F5C5'}
     response = requests.get(url, headers=headers)
-    return response.json()['rate']
+    return response.json()
 
-def send(alert, rate):
+def send(alert, response):
+    if alert.bigger_than == 1:
+        msg = ("""Hello %s.\n\nThis is an email to alert you that bitcoin """
+            """rate get beyond %s today %s""" %(
+            alert.owner, alert.value, response['time']))
+    else:
+        msg = ("""Hello %s.\n\nThis is an email to alert you that bitcoin """
+            """rate fell below %s today %s""" %(
+            alert.owner, alert.value, response['time']))
     send_mail(
         'CoinNotify',
-        'Examples Email message',
+        msg,
         'coinnotify42@gmail.com',
-        ['raphaeltin98@gmail.com'],
+        [alert.owner.email],
         fail_silently=False,
     )
-    #print("sending mail to %s for alert value = %f" % (alert.owner.email, alert.value) )
 
 def send_mail_if():
     response = get_rate_BTC()
-    #print("BTC rate = %d" % (response))
     queryset = models.Alert.objects.all()
     for alrt in queryset:
-        if (alrt.bigger_than == 1 and alrt.value < response) \
-        or (alrt.bigger_than == 0 and alrt.value > response):
+        if (alrt.bigger_than == 1 and alrt.value < response['rate']) \
+        or (alrt.bigger_than == 0 and alrt.value > response['rate']):
             send(alrt, response)
+            #delete alert
         
 
 def run():
